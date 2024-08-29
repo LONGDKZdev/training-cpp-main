76234abe333433d9974adab70a5b4352d456ff01
LONGDKZdev <longvo2549@gmail.com>
2024-08-29 21:06:35 +0700

    init

--- Day1/Test3.cpp
+++ Day1/Test3.cpp
@@ -6,12 +6,13 @@ int main()
 {
     cout << "6-7x=0" << endl;
     cout <<"valueout="<< (7.0/6)<< endl;
-    system("Pause");
     return 0;
-    // int nVarName;
-    // nVarName = 2000;
-    // cout << nVarName<< endl;
-    // system("Pause");
+
+    
+    int nVarName;
+    nVarName = 2000;
+    cout << nVarName<< endl;
+    system("Pause");
 }
 
 
