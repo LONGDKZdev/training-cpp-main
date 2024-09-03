dc64fafd499b80a3d1860285833ebd31cf61f77f
LONGDKZdev <longvo2549@gmail.com>
2024-09-03 19:50:42 +0700

    init

--- DEV/SetFill.cpp
+++ DEV/SetFill.cpp
@@ -2,6 +2,15 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
+
+void valueOut(string value)
+{
+if (value == "Value1")
+    cout << "Value1=100"<<endl;
+else if(value == "Value2")
+    cout << "value2=1000"<<endl;
+}
+
 int main()
 {
     cout << setfill('_');
@@ -11,4 +20,8 @@ int main()
     cout << setfill('_');
     cout << setw(25)<< "_" << endl;
     cout << setfill (' ');
+
+    valueOut("Value1");
+    valueOut("Value2");
+system("Pause");
 }
