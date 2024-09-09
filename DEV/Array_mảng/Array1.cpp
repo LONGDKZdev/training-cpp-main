aeacfd1b282c033d51ff1a0c7de98effb036f4ed
LONGDKZdev <longvo2549@gmail.com>
2024-09-09 22:42:09 +0700

    init

--- DEV/Array_mảng/Array1.cpp
+++ DEV/Array_mảng/Array1.cpp
@@ -7,11 +7,22 @@ using namespace std;
 int main ()
 {
     //khai báo
-    int A[6]; 
-    int A6[] = { 0,2,4,6,8,10 }; //khởi tạo
     // int A[6]; 
-    int A[] = { 0,2,4,6,8,10 }; //khởi tạo
+    // int A6[] = { 0,2,4,6,8,10 }; //khởi tạo
+    // cout << A6[4] <<endl;
+    int A[10];
+    int A10[]={1,2,3,4,5,6,7,8,9,10};
 
-    cout << A[1] <<endl;
+    cout << A10[0] <<endl; 
+    cout << A10[1] <<endl;
+    cout << A10[2] <<endl;
+    cout << A10[3] <<endl;
+    cout << A10[4] <<endl;
+    cout << A10[5] <<endl;
+    cout << A10[6] <<endl;
+    cout << A10[7] <<endl;
+    cout << A10[8] <<endl;
+    cout << A10[9] <<endl;
+
     system("Pause");
 }
