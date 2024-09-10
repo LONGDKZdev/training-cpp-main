baf5d41b1c5ccff31aeb2868ceadf65adb763cd5
LONGDKZdev <longvo2549@gmail.com>
2024-09-10 19:01:06 +0700

    init

---
+++ C++ Turtorial/BasicPrograming(Learing)/Period1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int valueA;
+    int valueB;
+    int Sum;
+
+    cout << "Enter the valueA =";
+    cin >> valueA;
+    cout << "Enter the valueB =";
+    cin >> valueB;
+    Sum= valueA +valueB;
+    cout << "valueOut ="<<Sum<<endl;
+
+system("Pause");
+}
