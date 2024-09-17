a8369d51493761a6757071c9936b168b2f82c7e5
LONGDKZdev <longvo2549@gmail.com>
2024-09-17 12:49:34 +0700

    init

--- DEV/Array_mảng/Array1.cpp
+++ DEV/Array_mảng/Array1.cpp
@@ -10,17 +10,23 @@ int main ()
     // int A[6]; 
     // int A6[] = { 0,2,4,6,8,10 }; //khởi tạo
     // cout << A6[4] <<endl;
-    int A[11]={0,1,2,3,4,5,6,7,8,9,10};
-    sizeof(int);
+    int A[11]={0,1,2,3,4,5,6,7,8,9,10}; 
     int valueA;
     string B[5]= { "one", "two", "three", "four", "five"};
+    int C[10]={1,3,5,7,9,11,13,15,17,19};
+
     int valueSizeA = sizeof(A)/ sizeof(A[0]);
     int valueSizeB = sizeof(B)/ sizeof(B[0]);
+    int valueSizeC = sizeof(C)/ sizeof(C[0]);
+    int value = A[2];
+
     cout << "Size of the array A ="<< valueSizeA <<endl;
     cout << "Size of the array B ="<< valueSizeB <<endl;
+    cout << "Size of the array C ="<< valueSizeC <<endl;
+    cout << "value ="<< value <<endl;
 
-    int M[]={0,10,20,30,40,50,60,70,80,90,100};
-    cout<< "value from array M before change ="<<M[0]<<endl;
+    int M[]={10,20,30,40,50,60,70,80,90,100};
+    cout<< "value from array M before change ="<<M[2]<<endl;
     M[0]=1000;
     cout<< "value from array M after change ="<<M[0]<<endl;
 
